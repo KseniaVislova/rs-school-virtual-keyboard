6a542dbe8a940e53bf6c81430e79fcc2fabd8eda
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-05 00:55:51 +0300

    feat: implement textarea to the page

--- src/js/content.js
+++ src/js/content.js
@@ -4,12 +4,14 @@ import { createEl , addHtml } from "./functions";
 const WRAPPER = document.querySelector('.wrapper');
 
 const content = () => {
-  let container = createEl('div', 'container', '')
+  let container = createEl('div', 'container', '');
 
   let title = createEl(data.title.tag, data.title.className, data.title.text);
+  let textarea = createEl('textarea', 'textarea', '')
 
   addHtml(WRAPPER, container);
   addHtml(container, title);
+  addHtml(container, textarea);
 };
 
 export default content;
