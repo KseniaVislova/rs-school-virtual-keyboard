d292adf72c994d3cb486f0cf92ee71f05ffc9d05
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-08 15:13:34 +0300

    feat: implement adding text to the textarea by virtual keyboard

--- src/js/keys.js
+++ src/js/keys.js
@@ -1,10 +1,21 @@
 const listenEvent = () => {
   const KEYS = document.querySelectorAll('.key');
+  const textarea = document.querySelector('.textarea');
+  let text = textarea.textContent;
+  let cursor = textarea.selectionStart;
 
   document.addEventListener('keydown', (event) => {
+
     KEYS.forEach(item => {
       if (item.getAttribute('data-key') === event.code) {
         item.classList.add('active');
+      }
+      if(!item.classList.contains('key-special') && item.getAttribute('data-key') === event.code) {
+        cursor = textarea.selectionStart;
+        event.preventDefault();
+        text += item.textContent;
+        textarea.textContent = text;
+        textarea.selectionStart = cursor + 1;
       }
     })
   })
@@ -16,6 +27,19 @@ const listenEvent = () => {
       }
     })
   })
+
+  KEYS.forEach(item => {
+    item.addEventListener('click', () => {
+      if(!item.classList.contains('key-special')) {
+        textarea.focus();
+        cursor = textarea.selectionStart;
+        text += item.textContent;
+        textarea.textContent = text;
+        textarea.selectionStart = cursor + 1;
+      }
+    })
+  })
+
 }
 
 export default listenEvent;
