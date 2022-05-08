c7ad764fc37a180bf73c3d167dd12b6c7cd519a1
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-05-08 21:51:35 +0300

    feat: implement switching languages by pressing shift + alt

--- src/js/content.js
+++ src/js/content.js
@@ -1,7 +1,57 @@
 import data from "./data";
-import { createEl , addHtml } from "./functions";
+import { createEl , addHtml, runShiftAlt } from "./functions";
 
 const BODY = document.body;
+let lang = 'en';
+let DATA = data.en;
+
+const createKeys = (keyboard, DATA) => {
+  keyboard.innerHTML = '';
+  for (let i in DATA) {
+    let keyRow = createEl('div', ['keyboard__inner']);
+    addHtml(keyboard, keyRow);
+    let arr = DATA[i];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].data !== undefined) {
+        let key = createEl('button', arr[i].classes, arr[i].key, arr[i].data);
+        addHtml(keyRow, key);
+      } else {
+        let key = createEl('button', arr[i].classes, arr[i].key);
+        addHtml(keyRow, key);
+      }
+    }
+  }
+}
+
+const codes = ["ShiftLeft", "AltLeft"];
+let pressed = new Set();
+
+document.addEventListener('keydown', (event) => {
+  pressed.add(event.code);
+
+  for (let code of codes) {
+    if (!pressed.has(code)) {
+      return;
+    }
+  }
+
+  if (lang === 'en') {
+    lang = 'ru';
+    DATA = data.ru;
+  } else {
+    lang = 'en';
+    DATA = data.en;
+  }
+
+  console.log('Изменение lang: ', lang)
+
+  let keyboard = document.querySelector('.keyboard');
+  createKeys(keyboard, DATA);
+})
+
+document.addEventListener('keyup', function(event) {
+  pressed.delete(event.code);
+});
 
 const generateContent = () => {
   let wrapper = createEl('div', ['wrapper']);
@@ -16,26 +66,10 @@ const generateContent = () => {
   addHtml(container, textarea);
   addHtml(container, keyboard);
 
-  const DATA = data.en;
-
-  for (let i in DATA) {
-    let keyRow = createEl('div', ['keyboard__inner']);
-    addHtml(keyboard, keyRow);
-    let arr = DATA[i];
-    for (let i = 0; i < arr.length; i++) {
-      //console.log(arr[i].data)
-      if (arr[i].data !== undefined) {
-        let key = createEl('button', arr[i].classes, arr[i].key, arr[i].data);
-        addHtml(keyRow, key);
-      } else {
-        let key = createEl('button', arr[i].classes, arr[i].key);
-        addHtml(keyRow, key);
-      }
-    }
-  }
+  createKeys(keyboard, DATA);
 
   let warning = createEl('div', ['warning']);
-  let warningText = createEl('p', ['warning__text'], 'Клавиатура создана на операционной системе Windows. Для переключения языка используйте комбинация: SHIFT + ALT.');
+  let warningText = createEl('p', ['warning__text'], 'Клавиатура создана на операционной системе Windows. Для переключения языка используйте комбинация: SHIFT + ALT(левые).');
 
   addHtml(container, warning);
   addHtml(warning, warningText);
